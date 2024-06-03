ece3f7343004012af2142f79b9a4b2433e213698
Professor <maxkauftman@yandex.ru>
2024-06-03 15:59:11 +0300

    Fix: фикс недочетов

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -10,7 +10,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
-import ru.practicum.shareit.exceptions.ValidationException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
